e94698654883c0d2f7a361b5c5347815ec8377a5
REVAN <revanescence33@gmail.cpm>
2024-04-10 15:36:20 +0530

    var

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("this is me")
+
+name = ("tony stark")
+age = 24
+price = 150.5
+x = null
+y = undefined
+isFollow = false;
+console.log(name,age,price,x, y,isFollow)
+
+// dyanamically tight languge, data type define on run time?
