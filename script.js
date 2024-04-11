32677aaf084f4e93de5535951f153ac3a9447892
REVAN <revanescence33@gmail.cpm>
2024-04-11 16:22:31 +0530

    dom

--- script.js
+++ script.js
@@ -6,6 +6,6 @@ const price = 150.5 // block scoped
 var x = null
 var y = undefined
 let isFollow = false;
-console.log(name,age,price,x, y,isFollow)
+console.log(name,age,price,x, y,isFollow )
 
 // dyanamically tight languge, data type define on run time?
